425f78da898742184dce2a5e9f462763dec3d04e
Laurent Bouquet <bouquet@ndlp.fr>
2021-09-09 12:03:53 +0200

    First commit

--- Personnage.php
+++ Personnage.php
@@ -1,11 +1,63 @@
 <?php
 // Présence du mot-clé class suivi du nom de la classe.
 
-class Personnage {
+class Personnage
+{
 
-  // Déclaration des attributs et méthodes ici.
+    // Déclaration des attributs et méthodes ici.
+    private $_nom = 'Inconnu'; // Son nom, par défaut 'Inconnu'.
+    private $_force = 50; // La force du personnage, par défaut à 50.
+    private $_experience = 1; // Son expérience, par défaut à 1.
+    private $_degats = 0; // Ses dégats par défaut
 
 
+    public function __construct($nom) {
+        $this->_nom = $nom;
+        print('<br/> Le personnage "' . $nom . '" est créé !');
+    }
 
+    public function definirForce($force)
+    {
+        $this->_force = $force;
+    }
+    
+    public function definirDegats($degats)
+    {
+        $this->_degats = $degats;
+    }
+    
+    public function afficherDegats()
+    {
+        return $this->_degats;
+    }
+
+    public function definirExperience($experience)
+    {
+        $this->_experience = $experience;
+    }
+
+    public function parler()
+    { // Nous déclarons une méthode dont le seul but est d'afficher un texte.
+        print('Je suis un personnage !');
+    }
+
+    // Une méthode qui frappera un personnage (suivant la force qu'il a).
+    public function frapper($adversaire)
+    {
+        // $adversaire->_degats += $this->_force;
+        $adversaire->_degats = $adversaire->_degats + $this->_force;
+        $this->gagnerExperience();
+    }
+
+    // Une méthode augmentant l'attribut $experience du personnage.
+    public function gagnerExperience()
+    {
+        $this->_experience++;
+    }
+
+    public function afficherExperience()
+    {
+        return $this->_experience;
+    }
 
 }
